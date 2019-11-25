99ea3f4616b8907bed248af711b61ab4131ae50f
Anson <anson@staff.pchome.co.th>
2019-11-25 09:36:49 +0800

    大量上架

---
+++ product-xlslist.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="description" content="PChome Online Inc. เป็นกลุ่มบริษัทอีคอมเมิร์ซที่ใหญ่ที่สุดในไต้หวัน ขณะนี้เราได้เปิดให้บริการในประเทศไทยฟรีสำหรับทั้งผู้ขายและผู้ซื้อ คุณอยากจะขายหรืออยากจะซื้อของอะไรก็ได้ทั้งนั้นที่ PChome Thai ไม่ว่าจะเครื่องสำอาง, เสื้อผ้าแฟชั่น, เครื่องใช้ไฟฟ้า, หนังสือ, ของเล่น และอื่นๆ อีกมากมาย">
+<meta property="og:title" content="ออนไลน์ช้อปปิ้ง ที่ทุกสิ่งเป็นเรื่องแสนง่ายและสนุก" />
+<meta property="og:description" content="หาได้ทุกอย่างที่ต้องการที่ พีซีโฮมไทย - เครื่องสำอาง,เสื้อผ้าแฟชั่น, อุปกรณ์ไฟฟ้า, หนังสือ, ของเล่น และอื่นอีกมากมาย" />
+<meta property="og:image" content="http://cimg.pchome.co.th/fb/PchomeShop.jpg" />
+<meta property="og:url" content="http://www.pchome.co.th" />
+<meta property="og:site_name" content="PChome Thai" />
+
+<!-- favicon.ico-->
+<link rel="shortcut icon" type="image/x-icon" href="favicon/favicon.ico">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#e21e26">
+<meta name="msapplication-TileColor" content="#e21e26">
+<meta name="theme-color" content="#e21e26">
+
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="css/normalize.css">
+
+<!-- PAGE -->
+<link rel="stylesheet" type="text/css" href="css/product.css">
+
+<!-- Icon Font -->
+<link rel="stylesheet" type="text/css" href="css/fonts/pct-icon.css">
+<!-- BS grid -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+
+<title>PChome Thai | มุมลูกค้า</title>
+<!-- Google Tag Manager -->
+<script>
+(function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-52F2P7M');
+</script>
+<!-- End Google Tag Manager -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52F2P7M" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- 說明FOR RD: 填寫資料正確送出 請跑這一個區塊的LOADING-->
+    <div class="pct-ui loading-overlay-main" style="display: none;">
+        <div class="loading-circle-main">
+            <div class="circle-loader">
+                <div class="checkmark draw"></div>
+            </div>
+        </div>
+    </div>
+
+
+    <section id="pct-main">
+            <!-- PChome Thai Header -->
+               <?php include("assets/header.php"); ?>
+            <!-- PChome Thai Header end-->
+
+
+            <!-- PChome Thai Menu_mobile -->
+               <?php include("assets/menu-mobile.php"); ?>
+            <!-- PChome Thai Menu_mobile end-->
+            
+            <!-- PChome Thai Account Header -->
+
+            <!--說明For RD：手機版不出現-->
+            <div class="hidden-sm-down">
+               <?php include("assets/account-header.php"); ?>
+            </div>
+            <!-- PChome Thai Account Header end-->
+
+            <!-- 內容寬度 1200 區塊 -->
+            <div class="container pct-gutters container-min-height">
+                <div class="section-outer">
+
+                    <div class="row">
+                        <div class="col-md-3">
+                        <!-- PChome Thai Menu_pc -->
+                           <?php include("assets/menu-pc.php"); ?>
+                        <!-- PChome Thai Menu_pc end-->
+                        </div>
+                        <div class="col-md-9">
+        
+                            <section id="pct-section-content">
+
+                                <div class="pct-ui breadcrumb-back">
+                                    <span class="pct-icon icon-productedit"></span>
+                                    <ul>
+                                        <li><a href="index.php">首頁</a></li>
+                                        <li class="pct-icon icon-angle-right"></li>
+                                        <li>商品管理</li>
+                                        <li class="pct-icon icon-angle-right"></li>
+                                        <li>大量上架</li>
+                                    </ul>
+                                </div>
+
+                                 <!--商品大量上架-->
+                                    <div class="content-block product-description">
+                                       <div class="content-description">
+
+                                            <div class="xsl-download-wrap">
+                                               <div class="xsl-download-block">
+                                                    <div class="xsl-download">
+                                                      <a href="/seller_j/product/batchUpload">
+                                                        <div class="download-btn"><span class="pct-icon icon-download"></span></div>
+                                                        <img src="img/icon-bulklist-xsl.svg" alt="" class="img-fluid">
+                                                      </a>
+                                                    </div>
+                                               </div>
+                                                
+                                                <div class="xsl-download-text btn-center">
+                                                 <p class="description-title">商品大量上架</p> 
+                                                <p class="description-text">透過excel上架功能，只要賣家按照欄位說明填入，即可將您上架資料，快速刊登至賣場中。<a class="/sellers/batchprod/downloadxls">立刻下載範本</a> 或查看更多<a class="xlsstepBtn">上傳說明</a>。 </p> 
+                                                <p class="description-text">填寫完畢後，請將檔案以csv格式轉存。上傳檔案，請上傳您的csv檔及相關圖檔。請務必將所有檔案壓縮成zip檔上傳(壓縮檔內不可含資料夾)。圖檔格式只接受.jpg或.gif ，圖檔名稱請勿使用中文，請使用英文小寫。</p>
+                                                </div>
+                                            </div>   
+                                        
+                                            <div class="xsl-file-drop-area">
+                                                 <div class="pct-ui loading-overlay" style="display: none;">
+                                                    <div class="loading-circle medium"></div>
+                                                </div>                                               
+                                                <div class="file-drop-wrap">
+                                                    <div class="pct-icon icon-cloud-uploading"></div>  
+                                                  <a class="fake-btn pct-ui btn-sm btn-cta">選擇檔案</a>
+                                                  <div class="file-msg">或 直接拖曳 Zip 檔案到此上傳</div>
+                                                  <input class="file-input" type="file" multiple>
+                                                </div>
+
+                                            </div>
+
+                                       </div>
+
+                                </div>
+                                        
+                                <!--商品詳細描述-->
+                                <div class="content-block product-description">
+                                   <div class="content-title">大量上架紀錄</div>
+                                   <div class="content-description">
+                                      <p class="description-text">以下為excel大量上架紀錄列表，上傳資料後需2-3個工作天處理，上傳結果會在列表中紀錄，紀錄只會保留最後20筆資料。</p> 
+                                      <a class="pct-ui btn-sm xlserrorBtn"><li class="pct-btn-icon icon-information"></li>上傳結果錯誤說明</a>
+
+                                    <div class="bulklist-list-table lg-break-table">
+                                        <table class="normal-table">
+                                            <colgroup>
+                                                <col class="table--w20">
+                                                <col class="table--w20">
+                                                <col class="table--w15">
+                                                <col class="table--w10">
+                                                <col class="table--w10">
+                                                <col class="table--w10">
+                                                <col class="table--w15">
+                                            </colgroup>
+                                            <thead class="normallist-head">
+                                                <tr>
+                                                    <th><!-- ไฟล์ที่อัปโหลด -->Upload File Name</th>
+                                                    <th><!-- วันที่อัปโหลด -->Upload Date</th>
+                                                    <th><!-- สถานะอัปโหลด -->Upload Status</th>
+                                                    <th><!-- จำนวนครั้งที่อัปโหลด -->Total</th>
+                                                    <th><!-- สำเร็จ -->Success</th>
+                                                    <th><!-- ไม่สำเร็จ -->Failure</th>
+                                                    <th><!-- ผลการอัปโหลด -->Upload result</th>
+                                                </tr>
+                                            </thead>
+
+                                            <!--說明FOR RD: 沒有任何上傳紀錄時 -->
+                                            <tbody style="display: none;">
+                                                <tr class="table_default_empaty">
+                                                    <td colspan="7">
+                                                    <div class="message-text">沒有大量上傳紀錄</div></td>
+                                                </tr>
+                                            </tbody>
+                                            <tbody>
+                                                <tr class="normallist-item">
+                                                    <td>
+                                                       <span class="eng-num">proexcel_zh.zip</span>
+                                                    </td>
+                                                    <td data-title="上傳時間">
+                                                        <span class="eng-num">2017/12/19 <br class="hidden-md-down">15:16:32
+                                                    </td>
+                                                    <td data-title="上傳狀態">
+                                                       上傳失敗
+                                                    </td>
+                                                    <td data-title="檔案數">
+                                                      <span class="eng-num">10</span>
+                                                    </td>
+                                                    <td data-title="成功">
+                                                      <span class="eng-num">5</span>
+                                                    </td>
+                                                    <td data-title="失敗">
+                                                      <span class="eng-num">4</span>
+                                                    </td>
+                                                    <td data-title="上傳結果">
+                                                       <a class="pct-ui btn-xs">上傳結果</a>
+                                                    </td>
+                                                </tr>
+                                                <tr class="normallist-item">
+                                                    <td>
+                                                       <span class="eng-num">proexcel_zh.zip</span>
+                                                    </td>
+                                                    <td data-title="上傳時間">
+                                                        <span class="eng-num">2017/12/19 <br class="hidden-md-down"> 15:16:32
+                                                    </td>
+                                                    <td data-title="上傳狀態">
+                                                       處理完畢
+                                                    </td>
+                                                    <td data-title="檔案數">
+                                                      <span class="eng-num">10</span>
+                                                    </td>
+                                                    <td data-title="成功">
+                                                      <span class="eng-num">10</span>
+                                                    </td>
+                                                    <td data-title="失敗">
+                                                      <span class="eng-num">0</span>
+                                                    </td>
+                                                    <td data-title="上傳結果">
+                                                       <a class="pct-ui btn-xs" href="">上傳結果</a>
+                                                    </td>
+                                                </tr>
+                                                <tr class="normallist-item">
+                                                    <td>
+                                                       <span class="eng-num">proexcel_zh.zip</span>
+                                                    </td>
+                                                    <td data-title="上傳時間">
+                                                        <span class="eng-num">2017/12/19 <br class="hidden-md-down"> 15:16:32
+                                                    </td>
+                                                    <td data-title="上傳狀態">
+                                                        等待處理
+                                                    </td>
+                                                    <td data-title="檔案數">
+                                                      <span class="eng-num">0</span>
+                                                    </td>
+                                                    <td data-title="成功">
+                                                      <span class="eng-num">0</span>
+                                                    </td>
+                                                    <td data-title="失敗">
+                                                      <span class="eng-num">0</span>
+                                                    </td>
+                                                    <td data-title="上傳結果">
+                                                       --
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                            </table>  
+                                    </div>
+
+                                    <!-- PC 版 列出所有頁碼 -->
+                                    <div class="pct-ui page-pagination">
+                                        <ul class="eng-num">
+                                            <li class="active">1</li>
+                                            <li><a href="#">2</a></li>
+                                            <li><a href="#">3</a></li>
+                                            <li><a href="#">4</a></li>
+                                            <li><a href="#">5</a></li>
+                                            <li><a href="#">6</a></li>
+                                            <li><a href="#">7</a></li>
+                                            <li><a href="#">8</a></li>
+                                            <li class="pagination-jump-next"></li>
+                                            <li>
+                                                <a class="pagination-next" href="#">
+                                                    <span class="pct-icon icon-angle-right"></span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+
+
+
+                                    <!-- 手機版 Load more-->
+
+                                    <div class="pct-btn-loadcontent hidden-xs-up">
+                                        <a href="#" class="pct-ui btn btn-cta btn-load">LOAD MORE</a>
+                                    </div>  
+                                    
+                                   </div>
+
+                                </div>  
+                            </section>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- 內容寬度 1200 區塊 end-->
+
+
+        <!-- PChome Thai footer -->
+           <?php include("assets/footer.php"); ?>
+        <!-- PChome Thai footer end-->
+
+    </section>   
+
+
+
+<!-- PChome Thai lightBox -->
+<section id="pct-lightbox">
+    <div class="pct-lightBox_backGround"></div>
+    <div class="pct-lightBox_inner">
+        <?php include("assets/lightbox/global.php"); ?>
+        <?php include("assets/lightbox/product/lightbox-product.php"); ?>
+    </div>
+</section>
+<!-- PChome Thai lightBox end-->
+
+
+<!-- PChome Thai JS -->
+    <?php include("assets/js-include.php"); ?>
+<!-- PChome Thai JS end-->
+
+
+<!--商品管理 JS-->
+<script type="text/javascript" src="js/js_product.js"></script>
+<script>
+    $(document).ready(function() {
+      var menuItem = $('.sidebar-panel-wrap > ul > li:nth-child(4)');
+      menuItem.find('ul').show();
+      menuItem.addClass('active');
+    });
+</script>
+
+</body>
+</html>
